d812e74911232f72990b4dc649b00c5e050815a2
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2022-01-14 22:55:18 +0530

    initial commit

---
+++ src/main/java/com/namankhurpia/synonymit/SynonymitApplication.java
@@ -0,0 +1,13 @@
+package com.namankhurpia.synonymit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynonymitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynonymitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namankhurpia/synonymit/SynonymitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namankhurpia.synonymit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynonymitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
